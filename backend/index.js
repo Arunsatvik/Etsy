53fcd3061a039e56b680f5ae9776cd6289b34fb7
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2022-03-21 04:37:55 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors());
 //app.options('*', cors());
-app.use(cors({ origin: "http://localhost:3000/", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000/", credentials: false }));
 app.use(function(req, res, next) 
        { 
          res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); 
