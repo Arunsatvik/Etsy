dcd87646103ae169019928e17e96ea423a130db2
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2022-03-21 04:13:22 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,13 @@ const app = express();
 
 app.use(cors());
 //app.options('*', cors());
-//app.use(cors({ origin: "http://3.229.33.242:3000/", credentials: true }));
-// app.use(function(req, res, next) 
-//        { 
-//          res.header("Access-Control-Allow-Origin", "http://3.229.33.242:3000/"); 
-//          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
-//         next(); 
-//        });
+app.use(cors({ origin: "http://3.229.33.242:3000/", credentials: true }));
+app.use(function(req, res, next) 
+       { 
+         res.header("Access-Control-Allow-Origin", "http://3.229.33.242:3000/"); 
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+        next(); 
+       });
 
 
 // const db = mysql.createConnection({
