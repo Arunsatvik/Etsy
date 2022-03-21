289a111d984ab734458a4e8283306e547b141ae0
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2022-03-21 03:38:20 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.options('*', cors());
 app.use(cors({ origin: "http://3.229.33.242:3000/", credentials: true }));
 app.use(function(req, res, next) 
        { 
-         res.header("Access-Control-Allow-Origin", "*"); 
+         res.header("Access-Control-Allow-Origin", "http://3.229.33.242:3000/"); 
          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
         next(); 
        });
