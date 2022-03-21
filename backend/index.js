9a77d42d6d80e64c83a50ffd29d6a2e9ad9eda5f
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2022-03-21 02:28:09 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const fs = require("fs");
 
 const app = express();
+app.options('*', cors());
 app.use(cors({ origin: "http://3.229.33.242:3000/", credentials: true }));
 app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "http://3.229.33.242:3000/"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
